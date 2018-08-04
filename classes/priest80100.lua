d261c65bd292b610018c6ccc2470c9272377e67c
cryonex <cryonex@outlook.com>
2018-08-03 18:51:19 -0600

    Priest Void Bolt icon and localization fixed

--- classes/priest80100.lua
+++ classes/priest80100.lua
@@ -18,24 +18,55 @@ lib.classes["PRIEST"][3] = function()
 	lib.SetAltPower("Mana")
 	lib.InitCleave()
 
-	cfg.talents = lib.ScanTalents()
-	lib.ScanSpells()
+	cfg.talents = {
+		["Fortress of the Mind"]=IsPlayerSpell(193195),
+		["Shadowy Insight"]=IsPlayerSpell(162452),
+		["Shadow Word: Void"]=IsPlayerSpell(205351),
+		["Body and Soul"]=IsPlayerSpell(64129),
+		["San'layn"]=IsPlayerSpell(199855),
+		["Mania"]=IsPlayerSpell(193173),
+		["Twist of Fate"]=IsPlayerSpell(109142),
+		["Misery"]=IsPlayerSpell(238558),
+		["Dark Void"]=IsPlayerSpell(263346),
+		["Last Word"]=IsPlayerSpell(263716),
+		["Mind Bomb"]=IsPlayerSpell(205369),
+		["Psychic Horror"]=IsPlayerSpell(64044),
+		["Auspicious Spirits"]=IsPlayerSpell(155271),
+		["Shadow Word: Death"]=IsPlayerSpell(32379),
+		["Shadow Crash"]=IsPlayerSpell(205385),
+		["Lingering Insanity"]=IsPlayerSpell(199849),
+		["Mindbender"]=IsPlayerSpell(200174),
+		["Void Torrent"]=IsPlayerSpell(263165),
+		["Legacy of the Void"]=IsPlayerSpell(193225),
+		["Dark Ascension"]=IsPlayerSpell(280711),
+		["Surrender to Madness"]=IsPlayerSpell(193223),
+	}
 
-	lib.AddAura("Shadow Word: Pain", 589, "debuff", "target")
-	lib.AddAura("Shadowform", 232698, "buff", "player")
-	lib.AddAura("Vampiric Touch", 34914, "debuff", "target")
+	lib.AddSpell("Dark Ascension", {280711})
+	lib.AddSpell("Mind Blast", {8092})
+	lib.AddSpell("Mind Flay", {15407})
+	lib.AddSpell("Mind Sear", {48045})
+	lib.AddSpell("Mindbender", {200174})
+	lib.AddSpell("Shadow Crash", {205385})
+	lib.AddSpell("Shadow Word: Death", {32379})
+	lib.AddSpell("Shadow Word: Pain", {589}, "target")
+	lib.AddSpell("Shadow Word: Void", {205351})
+	lib.AddSpell("Shadowfiend", {34433})
+	lib.AddSpell("Shadowform", {232698}, true)
+	lib.AddSpell("Surrender to Madness", {193223})
+	lib.AddSpell("Vampiric Embrace", {15286})
+	lib.AddSpell("Vampiric Touch", {34914}, "target")
+	-- Void Bolt uses Void Eruption's cooldown, but has a different icon
+	-- The new icon is set later in this function
+	lib.AddSpell("Void Bolt", {228260})
+	lib.AddSpell("Void Eruption", {228260})
+	lib.AddSpell("Void Torrent", {263165})
+
 	lib.AddAura("Voidform", 194249, "buff", "player")
 
-	lib.RemoveSpell("Smite")
-	lib.RemoveSpell("Void Bolt")
-	lib.RemoveSpell("Void Eruption")
-	lib.RemoveSpell("Shadowfiend")
-	lib.AddSpell("Mind Flay", {15407})
-	lib.AddSpell("Shadow Word: Void", {205351})
-	lib.AddSpell("Void Bolt", {228266})
-	lib.AddSpell("Void Eruption", {228260})
-	lib.AddSpell("Shadowfiend", {34433})
-	lib.AddSpell("Mindbender", {200174})
+	-- Icon fix for Void Bolt
+	cfg.spells["Void Bolt"].realid = 228266
+	lib.SetSpellIcon("Void Bolt")
 
 	cfg.voidform_cost = 90
 	if cfg.talents["Legacy of the Void"] then
@@ -125,7 +156,7 @@ lib.classes["PRIEST"][3] = function()
 		end,
 		["Void Bolt"] = function()
 			if not lib.UnitHasAura("player", "Voidform") then return nil end
-			return lib.SimpleCDCheck("Void Eruption")
+			return lib.SimpleCDCheck("Void Bolt")
 		end,
 		["Shadow Word: Void"] = function()
 			if cfg.cleave_targets > 5 then return nil end
